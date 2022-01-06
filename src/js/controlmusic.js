84c6e6816fb443300b0b01c9afa5cfd7e2442e84
t2dev <thang.mk7@gamil.com>
2022-01-06 23:03:18 +0700

    6/1/2022 fix header and controlMusic

--- src/js/controlmusic.js
+++ src/js/controlmusic.js
@@ -3,11 +3,36 @@ const $$ = document.querySelectorAll.bind(document);
 
 const controlmusicCustomToggle = $(".controlmusic__custom-toggle");
 const controlmusicCustom = $(".controlmusic__custom");
+const inputRangeProgress = $(".controlmusic__progress input[type='range']");
+const inputRangeVolume = $(".controlmusic__custom-volume input[type='range']");
+
 const controlmusic = {
+  getBackgroundSize: function (inputRange) {
+    var min = +inputRange.min || 0;
+    var max = +inputRange.max || 100;
+    var value = +inputRange.value;
+    return ((value - min) / (max - min)) * 100;
+  },
+  setBackgroundSize: function (inputRange) {
+    inputRange.style.setProperty(
+      "--background-size",
+      `${this.getBackgroundSize(inputRange)}%`
+    );
+  },
   eventHandle: function () {
     controlmusicCustomToggle.addEventListener("click", function () {
       controlmusicCustom.classList.toggle("controlmusic__custom--show");
     });
+    //progress song
+    this.setBackgroundSize(inputRangeProgress);
+    inputRangeProgress.addEventListener("input", () =>
+      this.setBackgroundSize(inputRangeProgress)
+    );
+    // volume
+    this.setBackgroundSize(inputRangeVolume);
+    inputRangeVolume.addEventListener("input", () =>
+      this.setBackgroundSize(inputRangeVolume)
+    );
   },
   start: function () {
     this.eventHandle();
--- src/js/navmenu.js
+++ src/js/navmenu.js
@@ -2,27 +2,30 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const navmenuTabs = $$(".navmenu__tab");
 const navmenuTabSelect = $(".navmenu__tab-select");
+
+const contentApp = $(".content");
 const navmenu = {
-  tabActve: function (tab) {
+  tabActive: function (tab) {
     navmenuTabSelect.style.top = tab.offsetTop + "px";
     navmenuTabSelect.style.height = tab.offsetHeight + "px";
     tab.classList.add("navmenu__tab--active");
   },
+
   eventHandle: function () {
     // khi chọn 1 tab trong menu
     var currentIndex = 1;
-    this.tabActve(navmenuTabs[currentIndex]);
+    this.tabActive(navmenuTabs[currentIndex]);
+
     navmenuTabs.forEach(function (tab, index) {
       tab.addEventListener("click", function () {
         navmenuTabs[currentIndex].classList.remove("navmenu__tab--active");
         currentIndex = index;
-        navmenu.tabActve(navmenuTabs[currentIndex]);
+        navmenu.tabActive(navmenuTabs[currentIndex]);
       });
     });
     window.addEventListener("resize", function () {
-      navmenu.tabActve(navmenuTabs[currentIndex]);
+      navmenu.tabActive(navmenuTabs[currentIndex]);
     });
-    console.log(navmenuTabs);
   },
   start: function () {
     this.eventHandle();
